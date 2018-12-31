63c0faca7be8ac03bc4b26a52773d229941d0e20
Holden B <holden.bucher.92@gmail.com>
2018-12-30 20:17:57 -0600

    add portfolio view

--- server.js
+++ server.js
@@ -50,6 +50,13 @@ app.get("/about", (req, res) => {
   });
 });
 
+app.get("/portfolio", (req, res) => {
+  res.render("portfolio.hbs", {
+    pageTitle: "Portfolio Page",
+    welcomeMessage: "This is my portfolio page!"
+  });
+});
+
 app.get("/bad", (req, res) => {
   res.send({
     errorMessage: "Unable to handle request"
